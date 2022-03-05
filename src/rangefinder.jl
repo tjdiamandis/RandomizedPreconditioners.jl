bc0be30dcdf2932f18ce6151a1baddeb35a170c9
Theo Diamandis <tjdiamandis@gmail.com>
2022-03-04 17:11:11 -0800

    fix bug with q=0

--- src/rangefinder.jl
+++ src/rangefinder.jl
@@ -5,13 +5,13 @@ function rangefinder(A::AbstractMatrix{T}, r::Int; q::Int=0) where {T <: Number}
     if q == 0
         Ω = 1/sqrt(n) * randn(n, r)
         Y = A * Ω
-        Array(qr(Ω).Q)
+    else
+        Y = 1/sqrt(m) * randn(m, r)
+        for i in 1:q
+            Y = Array(qr(Y).Q)
+            Y = A*A'*Y
+        end
     end
-
-    Y = 1/sqrt(m) * randn(m, r)
-    for i in 1:q
-        Y = Array(qr(Y).Q)
-        Y = A*A'*Y
-    end
+    
     return Array(qr(Y).Q)
 end
